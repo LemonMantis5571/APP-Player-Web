00fbb27fd87f0c12ef41f6541d76f3c282da82c2
LemonMantis5571 <briter456@gmail.com>
2023-06-20 21:37:41 -0600

    Added progress bar and song selection

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// music controllers
 const playButton = document.querySelector('#playbutton');
 const fowardButton = document.querySelector('#fowardButton');
 const backwardButton = document.querySelector('#backwardButton');
@@ -6,6 +7,13 @@ const audio = document.querySelector('#musicPlayer');
 const songName = document.querySelector('#songName');
 const bandName = document.querySelector('#bandName');
 const songQuote = document.querySelector('#quote');
+const footer = document.querySelector('.footer');
+const slider = document.querySelector('#durationController');
+
+// playlist div
+const playlist = document.querySelector('#playlist');
+const playlistButton = document.querySelector('#playlist-icon');
+
 
 const songs = [
     {
@@ -20,12 +28,25 @@ const songs = [
         fileName: 'taeyeon',
         quote: "It's Too late"
     },
+    {
+        songName: "Vapors",
+        band: 'Another Now',
+        fileName: 'anothernow',
+        quote: "Walls closing in to form a cage"
+    },
+    {
+        songName: 'Dark Beach',
+        band: 'Pastel Ghost',
+        fileName: 'pastelghost',
+        quote: "Hold me while I'm dreaming"
+    }
+
 ];
 
 let songIndex = 0;
 let isPlaying = false;
-
-loadSong(songs[songIndex]);
+let updateSliderTime;
+let isShown = false;
 
 const fowardSong = () => {
     if (songIndex < songs.length - 1) {
@@ -44,16 +65,22 @@ const backwardSong = () => {
     }
 };
 
-function loadSong(song) {
-    audio.src = `assets/${song.fileName}.mp3`;
-    img.src = `imgs/${song.fileName}.webp`;
-    songName.innerHTML = `${song.songName}`;
-    bandName.innerHTML = `${song.band}`;
-    songQuote.innerHTML = `${song.quote}`;
+const togglePlaylist = () => {
+    if (isShown) {
+        playlist.classList.replace('block', 'hidden');
+        isShown = false;
+        return;
+    }
 
-}
+    isShown = true;
+    playlist.classList.replace('hidden', 'block');
 
 
+};
+
+playlistButton.addEventListener('click', () => {
+    togglePlaylist();
+})
 
 const pauseSong = () => {
     playButton.classList.replace("fa-pause", "fa-play");
@@ -77,6 +104,32 @@ playButton.addEventListener('click', () => {
     pauseSong();
 });
 
+
+const skipDuration = () => {
+    const skipTime = (slider.value / 100) * audio.duration;
+    audio.currentTime = skipTime;
+};
+
+const updateSlider = () => {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    slider.value = progress;
+}
+
+const resetSlider = () => {
+    slider.value = 0;
+}
+
+slider.addEventListener('change', () => {
+    skipDuration();
+});
+
+audio.addEventListener('ended', () => {
+    clearInterval(updateSliderTime);
+    resetSlider();
+    pauseSong();
+});
+
+
 fowardButton.addEventListener('click', () => {
     pauseSong();
     fowardSong();
@@ -89,3 +142,26 @@ backwardButton.addEventListener('click', () => {
     loadSong(songs[songIndex]);
 });
 
+playlist.addEventListener('click', (e) => {
+    const target = e.target;
+    if (target.matches('li')) {
+        const songId = target.id;
+        console.log(songId);
+        pauseSong();
+        loadSong(songs[songId - 1]);
+    }
+});
+
+
+function loadSong(song) {
+
+    audio.src = `assets/${song.fileName}.mp3`;
+    img.src = `imgs/${song.fileName}.webp`;
+    songName.innerHTML = `${song.songName}`;
+    bandName.innerHTML = `${song.band}`;
+    songQuote.innerHTML = `${song.quote}`;
+
+    updateSliderTime = setInterval(updateSlider, 1000);
+}
+
+loadSong(songs[songIndex]);
